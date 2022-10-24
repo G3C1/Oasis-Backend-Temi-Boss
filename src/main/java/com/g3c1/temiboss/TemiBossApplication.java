c8ff1952f3ca324ce84d19fe8421202bcdf3eb00
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-10-24 16:28:01 +0900

    :tada: 사장앱 시작
    ㅇㅇ

---
+++ src/main/java/com/g3c1/temiboss/TemiBossApplication.java
@@ -0,0 +1,13 @@
+package com.g3c1.temiboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemiBossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemiBossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g3c1/temiboss/TemiBossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g3c1.temiboss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemiBossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
