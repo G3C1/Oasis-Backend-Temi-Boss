aa480e368ef2916a2d47cb03761ab8e221da48b6
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-10-26 16:42:50 +0900

    :sparkles: baseTimeEntity
    dd

--- src/main/java/com/g3c1/temiboss/domain/purchase/domain/entity/Purchase.java
+++ src/main/java/com/g3c1/temiboss/domain/purchase/domain/entity/Purchase.java
@@ -2,6 +2,7 @@ package com.g3c1.temiboss.domain.purchase.domain.entity;
 
 import com.g3c1.temiboss.domain.food.domain.entity.Food;
 import com.g3c1.temiboss.domain.seat.domain.entity.Seat;
+import com.g3c1.temiboss.global.entity.BaseTimeEntity;
 import lombok.*;
 
 import javax.persistence.*;
@@ -11,7 +12,7 @@ import javax.persistence.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Purchase {
+public class Purchase extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
